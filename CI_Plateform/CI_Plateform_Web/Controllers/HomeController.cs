cfea8b912fd69ecd046acc6c93b93fdfca8a7afb
Meetkumar Thakkar <pct42@server7.com>
2023-02-10 17:37:23 +0530

    login page

---
+++ CI_Plateform/CI_Plateform_Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CI_Plateform_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CI_Plateform_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult login()
+        {
+            return View();
+        }
+        public IActionResult ForgotPsd()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
